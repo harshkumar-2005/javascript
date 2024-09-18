375d8333d7e735301496dee51a36327c45e79a2e
harshkumar-2005 <157991814+harshkumar-2005@users.noreply.github.com>
2024-09-19 02:07:57 +0530

    some basics

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+// const item={
+//     obejct:"bulb",
+//     price:"450",
+//     voltage:"220",
+//     frequency:"50"
+// }
+
+// const stud_detail={
+//     student_ID:NULL,
+//     student_phone:"",
+//     student_marks:""
+// }
