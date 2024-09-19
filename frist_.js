499bf492e21d2f388a17ba3e7664f0e2ecac2503
harshkumar-2005 <157991814+harshkumar-2005@users.noreply.github.com>
2024-09-19 06:09:54 +0530

    operators

--- frist_.js
+++ frist_.js
@@ -43,7 +43,7 @@ const item= {
     "someone":false,
     "ramesh":false,
     "gopi":69,
-    "dish":1500
+    "dish":1500,
 }
 // how to use object variable
 console.log("object variable:-")
@@ -51,6 +51,15 @@ console.log(item["harsh"])
 console.log(item["gopi"])
 console.log(item["adarsh"])
 console.log(item["dish"])
+
+const is_it_metal={
+    "iron":true,
+    "gold":true,
+    "silver":true,
+    "oxygen":false,
+    "money":false,
+}
+console.log(is_it_metal)
 
 
 //------------------------------------------------------------
@@ -60,10 +69,54 @@ let num =12
 console.log(stri+" "+num)
 console.log(typeof(stri))
 
-const dict={
-    iron:"metal in the chemistry table",
-    gold:"one of the rarest metal",
-    silver:"second rarest metal",
-    oxygen:"required to live or survive",
-    money:"most necessary to live and survive"
-}
+
+// ---------------------------------------------------------------
+
+// artimatic operator
+// + - * / %
+// ** = exponential (a^b)
+// ++ = increment
+// -- = decrement
+
+a=10;  // previously declared
+b=2;  // previously declared
+c=2;
+d=2;
+e=2;
+
+console.log("a + b = ",a + b)
+console.log("a - b = ",a - b)
+console.log("a / b = ",a / b)
+console.log("a * b = ",a * b)
+console.log("a ** b = ",a ** b)  //a^b
+
+console.log("b++ = ",b++) // printed 2 and then increment
+console.log("c-- = ",c--) // printed 2 and then decrement
+console.log("++d = ",++d) // printed incremented value first
+console.log("--e = ",--e) // printed decremented value first
+
+//--------------------------------------------------------------
+
+// assignment operator
+// +=, -=, *=, /=, **=
+let ass1=2;
+console.log(ass1+=5)
+console.log(ass1-=5)
+console.log(ass1*=5)
+console.log(ass1/=5)
+console.log(ass1**=5)  //x**y=
+
+//---------------------------------------------------------------
+
+// comparision operator
+// ==, !=, === (equal value and type), !== (not equal value or not equal type), <, >, <=, >=, ?(ternery operator)
+let com1=12;
+let com2=7;
+console.log(com1>com2)
+console.log(com1<com2)
+console.log(com1==com2)
+console.log(com1!=com2)
+console.log(com1===com2)
+console.log(com1!==com2)
+
+
